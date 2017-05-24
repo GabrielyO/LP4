1cce09875cc80eba6fc6420e48b7bfaafaecb9f6
GabrielyO <gaby.o.nascimento@gmail.com>
2017-05-24 02:11:55 -0300

    Form Cadastro Add

---
+++ TrabalhoBiblioteca/TrabalhoBiblioteca/FormCadastrarLivro.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TrabalhoBiblioteca
+{
+    partial class FormCadastrarLivro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txttitulo = new System.Windows.Forms.TextBox();
+            this.txtautor = new System.Windows.Forms.TextBox();
+            this.txteditora = new System.Windows.Forms.TextBox();
+            this.btnEnviarCadastro = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Título";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Autor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Editora";
+            // 
+            // txttitulo
+            // 
+            this.txttitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttitulo.Location = new System.Drawing.Point(127, 38);
+            this.txttitulo.Name = "txttitulo";
+            this.txttitulo.Size = new System.Drawing.Size(216, 26);
+            this.txttitulo.TabIndex = 3;
+            // 
+            // txtautor
+            // 
+            this.txtautor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtautor.Location = new System.Drawing.Point(127, 75);
+            this.txtautor.Name = "txtautor";
+            this.txtautor.Size = new System.Drawing.Size(216, 26);
+            this.txtautor.TabIndex = 4;
+            // 
+            // txteditora
+            // 
+            this.txteditora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txteditora.Location = new System.Drawing.Point(127, 116);
+            this.txteditora.Name = "txteditora";
+            this.txteditora.Size = new System.Drawing.Size(216, 26);
+            this.txteditora.TabIndex = 5;
+            // 
+            // btnEnviarCadastro
+            // 
+            this.btnEnviarCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarCadastro.Location = new System.Drawing.Point(191, 148);
+            this.btnEnviarCadastro.Name = "btnEnviarCadastro";
+            this.btnEnviarCadastro.Size = new System.Drawing.Size(102, 39);
+            this.btnEnviarCadastro.TabIndex = 6;
+            this.btnEnviarCadastro.Text = "Cadastrar";
+            this.btnEnviarCadastro.UseVisualStyleBackColor = true;
+            this.btnEnviarCadastro.Click += new System.EventHandler(this.btnEnviarCadastro_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(191, 184);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(102, 39);
+            this.btnVoltar.TabIndex = 7;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // FormCadastrarLivro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnEnviarCadastro);
+            this.Controls.Add(this.txteditora);
+            this.Controls.Add(this.txtautor);
+            this.Controls.Add(this.txttitulo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormCadastrarLivro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormCadastrarLivro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txttitulo;
+        private System.Windows.Forms.TextBox txtautor;
+        private System.Windows.Forms.TextBox txteditora;
+        private System.Windows.Forms.Button btnEnviarCadastro;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
---
+++ TrabalhoBiblioteca/TrabalhoBiblioteca/FormCadastrarLivro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TrabalhoBiblioteca
+{
+    public partial class FormCadastrarLivro : Form
+    {
+        public FormCadastrarLivro()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnEnviarCadastro_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand()
+            {
+                Connection = new MySqlConnection("Server=127.0.0.1;Database=biblioteca;Uid=root;Pwd="),
+                CommandText = "INSERT INTO livro (Título, Autor, Editora) VALUES (@titulo, @autor, @editora);"
+            };
+
+            Livro l = new Livro();
+            l.titulo = txttitulo.Text;
+            l.autor = txtautor.Text;
+            l.editora = txteditora.Text;
+
+            cmd.Parameters.AddWithValue("@titulo", l.titulo); 
+            cmd.Parameters.AddWithValue("@autor", l.autor);
+            cmd.Parameters.AddWithValue("@editora", l.editora);
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+            MessageBox.Show("Livro cadastrado com sucesso");
+        }
+
+        public void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
